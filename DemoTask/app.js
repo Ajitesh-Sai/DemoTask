88c13c5bfeccaaa89fa935c4adc2606db63c0bdc
Sathyasai Ajitesh <60847274+Ajitesh-Sai@users.noreply.github.com>
2020-08-22 18:06:57 +0530

    Update app.js

--- DemoTask/app.js
+++ DemoTask/app.js
@@ -7,7 +7,8 @@ const ordersDiv = document.getElementById('ordersDiv');
 const btnSignout = document.getElementById('btnSignout');
 const orders = document.getElementById("orders");
 
-
+// const database = firebase.database();
+// const rootRef = database.ref('orders');
 
 
 btnSignin.addEventListener('click', e => {
@@ -53,10 +54,6 @@ firebase.auth().onAuthStateChanged (firebaseUser => {
                     id: key
                 });
             }
-            const order = fetchedOrders[0];
-            // order.map(field => console.log(field));
-            console.log(fetchedOrders[0]["name"], typeof fetchedOrders);
-            // console.log(res);
             
             for(i=0; i<fetchedOrders.length; i++){
                 var orderEach = document.createElement("div");
@@ -80,12 +77,16 @@ firebase.auth().onAuthStateChanged (firebaseUser => {
                 tag = document.createElement("select");
 
                 let option1 = document.createElement("option");
+                option1.value="Placed";
                 option1.text="Placed";
                 let option2 = document.createElement("option");
+                option2.value="Shipped";
                 option2.text="Shipped";
                 let option4 = document.createElement("option");
+                option4.value="Return";
                 option4.text="Return";
                 let option3 = document.createElement("option");
+                option3.value="Delivered";
                 option3.text="Delivered";
                 
                 tag.add(option1);
@@ -97,13 +98,25 @@ firebase.auth().onAuthStateChanged (firebaseUser => {
                 orderEach.appendChild(tag);
 
                 let btn = document.createElement("button");
-                btn.innerHTML = "Publish Changes"
-                // btn.value("jhadb");
+                btn.name = i;
+                btn.innerHTML = "Publish Changes";
                 btn.className="btn btn-primary";
                 btn.style.width="170px";
-                btn.style.margin="5px"
+                btn.style.margin="5px";
+
+                btn.addEventListener('click', e=> {
+                    let updatedOrder = {
+                        orderId: fetchedOrders[0]["orderId"],
+                        name: fetchedOrders[0]["name"],
+                        tracking: tag.options[e.selectedIndex].value,
+                        user: fetchedOrders[0]["user"]
+                        }
+                    console.log("sadas", tag.options[e.selectedIndex].value);
+                })
 
                 orderEach.appendChild(btn);
+                orderEach.style.boxSizing = "border-box";
+                orderEach.style.border = "0.1px solid lightgray";
                 
                 orderEach.style.boxShadow="2px 2px 2px darkgray";
                 orderEach.style.margin = "10px";
@@ -120,7 +133,3 @@ firebase.auth().onAuthStateChanged (firebaseUser => {
         console.log('not logged in');
     }
 });
-
-let fetchedOrders = [];
-
-
